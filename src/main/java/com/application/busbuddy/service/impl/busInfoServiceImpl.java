2c5d397f3fe0a08ba3b914093c2ab6eeb2063b83
Pranali More <morepranali986@gmail.com>
2025-07-10 00:31:53 +0530

    Added dto file, repository and service.

---
+++ src/main/java/com/application/busbuddy/service/impl/busInfoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.application.busbuddy.service.impl;
+
+import com.application.busbuddy.dto.busInfoDTO;
+import com.application.busbuddy.service.busInfoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class busInfoServiceImpl implements busInfoService {
+    @Override
+    public String fetchBusData(busInfoDTO request) {
+        return "";
+    }
+}
