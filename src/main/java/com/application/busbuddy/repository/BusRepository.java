6fc967c1c09467cb43c14c955b4feb04ffa08c03
samarth pathak <pathaksamarth11@gmail.com>
2025-08-08 09:50:54 +0530

    Added required annotations and logic

--- src/main/java/com/application/busbuddy/repository/BusRepository.java
+++ src/main/java/com/application/busbuddy/repository/BusRepository.java
@@ -2,9 +2,11 @@ package com.application.busbuddy.repository;
 
 import com.application.busbuddy.model.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BusRepository extends JpaRepository<Bus, Long> {
     List<Bus> findByProviderId(Long providerId);
 
